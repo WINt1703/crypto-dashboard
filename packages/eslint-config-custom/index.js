bcf9f3a6df26046bf19e9d59fb832eb5bda836ac
Alex <41703864+WINt1703@users.noreply.github.com>
2022-11-16 18:08:47 +0100

    initialization project

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,23 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:unicorn/recommended",
+    "plugin:react-hooks/recommended",
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',    
+  ],
+  parser: "@typescript-eslint/parser",
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-curly-brace-presence': 'error',
+    'unicorn/prevent-abbreviations': ['error', { allowList: { props: true, Props: true } }],
+    'unicorn/prefer-module': "warn",
+    'unicorn/filename-case': ['error', { case: 'camelCase' }],
+    'no-console': 'warn',
+    'react/self-closing-comp': 'error',
+    'unicorn/prefer-node-protocol': 'off',
+    '@next/next/no-html-link-for-pages': 'off'
+  }
 };
